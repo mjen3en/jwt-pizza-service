297ce5c00f6105ef2a19ea320f0ce61d7b277060
Micah <batmanwillreturn@gmail.com>
2025-02-01 13:57:10 -0700

    implement login, register, and update tests for authRouter

---
+++ src/tests/authRouter.test.js
@@ -0,0 +1,70 @@
+const request = require('supertest');
+const app = require('../service');
+const e = require('express');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+let testUserId;
+
+beforeEach(async () => {
+  //register a new user and save token and id before each test
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  testUserId = registerRes.body.user.id;
+});
+
+afterEach(async () => {
+  // logout user after each test
+  const logout = await request(app).delete(`/api/auth/`).auth(testUserAuthToken, { type: 'bearer' });
+  console.log(logout.body);
+});
+
+test('register', async () => {
+    const newUser = { name: 'new diner', email: Math.random().toString(36).substring(2, 12) + '@test.com', password: 'b' };
+    const registerTest = await request(app).post('/api/auth').send(newUser);
+    expect(registerTest.status).toBe(200);
+    expect(registerTest.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+    const { password, ...user } = { ...newUser, roles: [{ role: 'diner' }] };
+    expect(registerTest.body.user).toMatchObject(user);
+  });
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  console.log('Login Response:', loginRes.body.user);
+  expect(loginRes.status).toBe(200);
+  testUserAuthToken = loginRes.body.token;
+  console.log(testUserAuthToken); 
+  expect(testUserAuthToken).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  console.log(loginRes.body.user);
+
+  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  expect(loginRes.body.user).toMatchObject(user);
+});
+
+test('update', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  testUserAuthToken = loginRes.body.token
+  console.log('Login Response:', loginRes.body.user);
+  const testUserID = loginRes.body.user.id;
+  console.log(testUserID);
+  expect(testUserID).not.toBeNull();
+
+  const updatedUser = { ...testUser, password: 'c' };
+  const updateRes = await request(app).put(`/api/auth/${testUserID}`).auth(testUserAuthToken, { type: 'bearer' }).send(updatedUser);
+  console.log("Updated User:", updatedUser);
+  console.log('Update Response:', updateRes.body);  
+  expect(updateRes.status).toBe(200);
+  const { password, ...userWithoutPassword } = updatedUser;
+  console.log("User without password:", userWithoutPassword);
+  console.log("Update Response Body:", updateRes.body);
+  expect(updateRes.body).toMatchObject(userWithoutPassword);
+
+
+
+  
+
+
+  // const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  // expect(loginRes.body.user).toMatchObject(user);
+});
