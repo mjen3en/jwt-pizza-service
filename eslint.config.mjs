9670e11b7e0743e3addd34e16ebcdcf2a3ae2e68
Micah <batmanwillreturn@gmail.com>
2025-02-05 22:49:50 -0700

    I'm a fool, buffoon even

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,7 @@ import pluginJs from "@eslint/js";
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
+  {languageOptions: { globals: globals.jest } },
   pluginJs.configs.recommended,
-  module.exports = {
-    env: {
-      jest: true
-    }
-  }
 ];
 
