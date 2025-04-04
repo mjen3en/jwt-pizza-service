da389d2e024cfca3b291fa13354f9959739ac0f4
Micah <batmanwillreturn@gmail.com>
2025-04-04 17:35:34 -0600

    ....

--- logging.js
+++ logging.js
@@ -53,14 +53,17 @@ class Logger {
 
   sendLogToGrafana(event) {
     const body = JSON.stringify(event);
-    fetch(`${config.logging.url}`, {
+    fetch(`${config.url}`, {
       method: 'post',
       body: body,
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${config.logging.apiKey}`,
+        Authorization: `Bearer ${config.userId}:${config.apiKey}`,
       },
-    })
+    }).then((res) => {
+      if (!res.ok) console.log('Failed to send log to Grafana');
+    });
   }
 }
+
 module.exports = new Logger();
